cc3cfab01dd590af1ea0336cdffdaf34ef801b91
Jhainey <milevisj@gmail.com>
2018-12-03 17:23:57 -0500

    dasdasd

--- experience/views.py
+++ experience/views.py
@@ -117,10 +117,7 @@ def convert_voice_to_text(f):
 
         audio = types.RecognitionAudio(content=content)
         config = types.RecognitionConfig(
-            encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-            sample_rate_hertz=16000,
-
-            language_code='es-ES')
+                        language_code='es-ES')
 
         # Detects speech in the audio file
         response = client.recognize(config, audio)
