ccaf0a282c6e18366170b90f25565191ab72b2ae
Jhainey <milevisj@gmail.com>
2018-12-03 17:11:51 -0500

    asdasdasd

--- experience/views.py
+++ experience/views.py
@@ -74,8 +74,8 @@ def RegisterExperience(request):
         uploaded_file = request.FILES.get('uploaded_file', '')
         uploaded_file2 = request.FILES.get('uploaded_file2', '')
 
-        print(request.FILES)
-        print(uploaded_file.size)
+        #print(request.FILES)
+       # print(uploaded_file.size)
        # print(audio1)
         text1 = ""
         text2 = ""
