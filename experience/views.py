b8532d98b02754e414947d8100a401690867cf68
fernando <fernandoga22@gmail.com>
2018-12-07 13:12:35 -0500

    subida del archivo a dropbox

--- experience/views.py
+++ experience/views.py
@@ -43,9 +43,9 @@ import os.path ##libreria que verifica si los archivos existen
 
 
 rutadropbox="C:/Users/fernando/Dropbox/demo/input/"
-rutadropbox="/root/Dropbox/demo/ouput"
-rutainputdropbox="./input/"
-rutaouputdropbox="./ouput/"
+rutadropbox="/root/Dropbox/demo/input"
+rutainputdropbox="/root/Dropbox/demo/input/"
+rutaouputdropbox="/root/Dropbox/demo/ouput/"
 ##el siguiente metodo retornara toda la data de experiencias y calls en formato json
 @api_view(['GET'])
 
@@ -67,7 +67,7 @@ def getexpandcalls(request):
     print(os.path.isdir(rutadropbox))
     content = {'experiences': experiences, 'calls': calls, 'success': 1}
     try:
-        with open('~Dropbox/demo/ouput/call.json', 'w') as outfile:
+        with open(rutainputdropbox+'call.json', 'w') as outfile:
             json.dump(content, outfile)
     except Exception:
         print ("No se subio el archivo call.json")
